97ec6da984dd703d44a5bae237b25c1b086991ee
JoanPalau <joanp4l4u@gmail.com>
2019-10-12 10:53:19 +0200

    feat: initial app commit

---
+++ chatApp/src/App.tsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import { Redirect, Route } from 'react-router-dom';
+import { IonApp, IonRouterOutlet, IonSplitPane } from '@ionic/react';
+import { IonReactRouter } from '@ionic/react-router';
+import { AppPage } from './declarations';
+
+import Menu from './components/Menu';
+import Home from './pages/Home';
+import List from './pages/List';
+import { home, list, logIn, logOut, person, chatbubbles } from 'ionicons/icons';
+
+/* Core CSS required for Ionic components to work properly */
+import '@ionic/react/css/core.css';
+
+/* Basic CSS for apps built with Ionic */
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/structure.css';
+import '@ionic/react/css/typography.css';
+
+/* Optional CSS utils that can be commented out */
+import '@ionic/react/css/padding.css';
+import '@ionic/react/css/float-elements.css';
+import '@ionic/react/css/text-alignment.css';
+import '@ionic/react/css/text-transformation.css';
+import '@ionic/react/css/flex-utils.css';
+import '@ionic/react/css/display.css';
+
+/* Theme variables */
+import './theme/variables.css';
+
+const appPages: AppPage[] = [
+  {
+    title: 'Home',
+    url: '/home',
+    icon: home
+  },
+  {
+    title: 'List',
+    url: '/home/list',
+    icon: list
+  },
+  {
+    title: 'Login',
+    url: '/login',
+    icon: logIn
+  },
+  {
+    title: 'Logout',
+    url: '/logout',
+    icon: logOut
+  },
+  {
+    title: 'Profile',
+    url: '/profile',
+    icon: person
+  },
+  {
+    title: 'Chat',
+    url: '/chat',
+    icon: chatbubbles
+  }
+];
+
+const App: React.FC = () => (
+  <IonApp>
+    <IonReactRouter>
+      <IonSplitPane contentId="main">
+        <Menu appPages={appPages} />
+        <IonRouterOutlet id="main">
+          <Route path="/home" component={Home} exact={true} />
+          <Route path="/home/list" component={List} exact={true} />
+          <Route exact path="/" render={() => <Redirect to="/home" />} />
+        </IonRouterOutlet>
+      </IonSplitPane>
+    </IonReactRouter>
+  </IonApp>
+);
+
+export default App;
