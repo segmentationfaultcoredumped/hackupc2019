f12b827689f4757d91931bbd2e0e1011e0bbdba1
JoanPalau <joanp4l4u@gmail.com>
2019-10-12 23:15:11 +0200

    feat: added react app

--- graphQL/resolvers/users.js
+++ graphQL/resolvers/users.js
@@ -18,6 +18,10 @@ module.exports = {
     Mutation: {
         async login(_, {username, password }){
             const {errors, valid} = validateLoginInput(username, password);
+
+            if(!valid) {
+                throw new UserInputError('Errors', { errors });
+            }
             const user = await User.findOne({ username });
             if(!user) {
                 errors.general = 'User not found';
